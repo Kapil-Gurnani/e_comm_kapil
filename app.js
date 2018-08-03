c4c6c2b1c858f5d90a6bde3f891b596b1abaefe6
Kapil-Gurnani <kapilgurnani4@gmail.com>
2018-08-03 13:54:47 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+
+var app = express();
+
+app.set("engine",hbs);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+
+    // res.send('<h1>Hello Express!!!!!</h1>');
+    res.render('index.hbs');
+});
+
+app.listen(3000);
